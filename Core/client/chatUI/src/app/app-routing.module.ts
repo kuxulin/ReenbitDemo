4285ae0127fecab317d8acdc0c3a20e54f2c0a16
Maksym Moroz <maskmoroz@gmail.com>
2024-08-19 16:11:31 +0300

    configured routes

--- Core/client/chatUI/src/app/app-routing.module.ts
+++ Core/client/chatUI/src/app/app-routing.module.ts
@@ -3,16 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ChatListComponent } from './chat-list/chat-list.component';
 import { RegisterComponent } from './register/register.component';
+import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'chats', component: ChatListComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'chats/:chatId', component: ChatComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { bindToComponentInputs: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
