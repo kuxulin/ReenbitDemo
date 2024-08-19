cc6d2efd99ada4fed82e23c350119af594286e93
Maksym Moroz <maskmoroz@gmail.com>
2024-08-19 16:12:51 +0300

    added list of chats with particular chat view

--- Core/client/chatUI/src/app/chat-list/chat-list.component.ts
+++ Core/client/chatUI/src/app/chat-list/chat-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import Chat from '../models/Chat';
 import { ChatsService } from '../services/chats.service';
+import { CreateNewChatDialogComponent } from '../create-new-chat-dialog/create-new-chat-dialog.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-chat-list',
@@ -11,9 +13,15 @@ import { ChatsService } from '../services/chats.service';
 export class ChatListComponent implements OnInit {
   chats$!: Observable<Chat[]>;
 
-  constructor(private chatsService: ChatsService) {}
+  constructor(private dialog: MatDialog, private chatsService: ChatsService) {}
 
   ngOnInit(): void {
     this.chats$ = this.chatsService.loadAllUserChats();
+  }
+
+  openCreatingChatDialog() {
+    let dialogref = this.dialog.open(CreateNewChatDialogComponent);
+
+    dialogref.afterClosed().subscribe(); // update chatlist
   }
 }
--- Core/client/chatUI/src/app/chat/chat.component.ts
+++ Core/client/chatUI/src/app/chat/chat.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import Chat from '../models/Chat';
+import { ChatsService } from '../services/chats.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  @Input()
+  chatId!: string;
+  chat$!: Observable<Chat>;
 
+  constructor(private chatsService: ChatsService) {}
+
+  ngOnInit(): void {
+    this.chat$ = this.chatsService.getChatById(this.chatId);
+  }
 }
