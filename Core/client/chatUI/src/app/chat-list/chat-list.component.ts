08ed05fce2c805d9223ecd18146776dc798689bb
Maksym Moroz <maskmoroz@gmail.com>
2024-08-20 09:56:32 +0300

    added appropriate interlocutor name showing

--- Core/client/chatUI/src/app/chat-list/chat-list.component.ts
+++ Core/client/chatUI/src/app/chat-list/chat-list.component.ts
@@ -4,6 +4,7 @@ import Chat from '../models/Chat';
 import { ChatsService } from '../services/chats.service';
 import { CreateNewChatDialogComponent } from '../create-new-chat-dialog/create-new-chat-dialog.component';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-chat-list',
@@ -13,7 +14,11 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 export class ChatListComponent implements OnInit {
   chats$!: Observable<Chat[]>;
 
-  constructor(private dialog: MatDialog, private chatsService: ChatsService) {}
+  constructor(
+    private dialog: MatDialog,
+    private chatsService: ChatsService,
+    private usersService: UsersService
+  ) {}
 
   ngOnInit(): void {
     this.chats$ = this.chatsService.loadAllUserChats();
@@ -21,7 +26,11 @@ export class ChatListComponent implements OnInit {
 
   openCreatingChatDialog() {
     let dialogref = this.dialog.open(CreateNewChatDialogComponent);
-
     dialogref.afterClosed().subscribe(); // update chatlist
+  }
+
+  showInterlocutorName(firstUserName: string, secondUserName: string) {
+    let user = this.usersService.getCurrentUser();
+    return user.userName === firstUserName ? secondUserName : firstUserName;
   }
 }
